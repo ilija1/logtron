3f1c3e6a68da051e6db68ae9e86c1f6bd06d9e72
Ilija Stevcev <ilija1@users.noreply.github.com>
2020-09-18 12:23:36 -0400

    add option to not use deepcopy for configs (#9)

--- logtron/config/discover_config.py
+++ logtron/config/discover_config.py
@@ -36,6 +36,10 @@ def discover_config(existing=None):
     if isinstance(existing, str):
         config.update(read_config_file(existing))
     else:
-        config.update(deepcopy(existing))
+        use_deepcopy = existing.get("use_deepcopy", True)
+        if use_deepcopy:
+            config.update(deepcopy(existing))
+        else:
+            config.update(existing)
 
     return config
