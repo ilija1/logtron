51241739b49c7f8000f2514aff590bdce935357e
Ilija Stevcev <ilija1@users.noreply.github.com>
2020-09-19 23:35:15 -0400

    New docs (#13)

---
+++ docs-src/pages/content/_slug.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="section">
+    <article>
+      <h1 class="title">{{ page.title }}</h1>
+      <nuxt-content :document="page" />
+    </article>
+    <hr />
+    <div class="field">
+      <b-tag rounded>Last updated {{ formatDate(page.updatedAt) }}</b-tag>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const page = await $content(params.slug).fetch()
+
+    return {
+      page,
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return new Date(date).toLocaleString('en')
+    },
+  },
+}
+</script>
